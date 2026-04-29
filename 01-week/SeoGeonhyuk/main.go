7deece6d57cfb1cf750bc12eb1220e46c5f3b414
SeoGeonhyuk <sgh4123@naver.com>
2026-04-29 08:37:10 +0000

    feat: implementation separate network namespace function

--- 01-week/SeoGeonhyuk/main.go
+++ 01-week/SeoGeonhyuk/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"syscall"
+)
+
 func main() {
 	return
+}
+
+func createNSContainer(path string, args []string) (int, error) {
+	if path != "/bin/sleep" {
+		return -1, fmt.Errorf("path must be /bin/sleep")
+	}
+	pid, err := syscall.ForkExec(path, args, &syscall.ProcAttr{
+		Sys: &syscall.SysProcAttr{
+			Cloneflags: syscall.CLONE_NEWNS,
+		},
+	})
+	if err != nil {
+		return -1, err
+	}
+	return pid, nil
 }
