35f1addd7328055d10afdeb5c7e2944717e5f104
Hyungsub (Danny) Kim <aa_aa_aa_11@naver.com>
2026-05-03 21:30:50 +0900

    feat: week01 task

---
+++ 01-week/khs-alt/service.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"syscall"
+
+	"golang.org/x/sys/unix"
+)
+
+func MakeProcess(path string, args []string) (*Output, error) {
+	runtime.LockOSThread()
+	// 고루틴이 하나의 os thread만 점유하도록 하기 위해 runtime.UnlockOSThread() 호출을 안함.
+	// defer runtime.UnlockOSThread()
+
+	parentPid := os.Getpid()
+	err := unix.Unshare(unix.CLONE_NEWNET)
+	if err != nil {
+		return nil, err
+	}
+	childPid, err := syscall.ForkExec(path, args, nil)
+	if err != nil {
+		return nil, err
+	}
+	tid := syscall.Gettid()
+	fmt.Printf("thread id: %d\n", tid)
+
+	output := Output{
+		ParentPid: parentPid,
+		ChildPid:  childPid,
+	}
+	return &output, nil
+}
