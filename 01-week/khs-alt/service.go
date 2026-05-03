1aaa7f1ca100d2ff75713eb869a99be045aef080
Hyungsub (Danny) Kim <aa_aa_aa_11@naver.com>
2026-05-03 21:50:59 +0900

    fix: add argv[0] to ForkExec

--- 01-week/khs-alt/service.go
+++ 01-week/khs-alt/service.go
@@ -14,12 +14,14 @@ func MakeProcess(path string, args []string) (*Output, error) {
 	// 고루틴이 하나의 os thread만 점유하도록 하기 위해 runtime.UnlockOSThread() 호출을 안함.
 	// defer runtime.UnlockOSThread()
 
+	argv := append([]string{path}, args...)
+
 	parentPid := os.Getpid()
 	err := unix.Unshare(unix.CLONE_NEWNET)
 	if err != nil {
 		return nil, err
 	}
-	childPid, err := syscall.ForkExec(path, args, nil)
+	childPid, err := syscall.ForkExec(path, argv, nil)
 	if err != nil {
 		return nil, err
 	}
